8fa4397ed4c1b4eb29d94ddd2a5fbf2a22689f2e
Michael Li <mikelisfbay@yahoo.com>
2019-04-22 13:02:45 -0700

    send real data

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from umqtt.simple import MQTTClient
 repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 
 
-def main(clientID = "umqtt_client", server = "mosquitto.org"):
+def main(clientID = "umqtt_client", server = "mosquitto.org", temp=75):
 	c = MQTTClient(clientID, server)
 	print ('client connect status :')
 	print(c.connect())
-	c.publish(b"xyzabc/fahrenheit", b"72")
+	c.publish(b"xyzabc/fahrenheit", str(temp))
 	c.disconnect()
 
 print ("Python name : %s." % __name__)
@@ -22,11 +22,11 @@ print ("Python name : %s." % __name__)
 if __name__ == "__main__":
 	print ("SHT31 I2C Example")
 	sht31 = SHT31_Sensor(freq = 100000,sdapin = 21, sclpin = 22)
-	measure_data = sht31.read_temp_humd()
-	print (measure_data)
 	measure_data = sht31.read_temp_humd(fahreheit = False)
 	print (measure_data)
-	main(clientID = '1234')
+	measure_data = sht31.read_temp_humd()
+	print (measure_data)
+	main(clientID = '1234',temp=measure_data[0])
 
 
 # Wait for button 0 to be pressed, and then exit
